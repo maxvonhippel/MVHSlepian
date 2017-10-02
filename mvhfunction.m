a28b00a652b73913bde40a83c32ec6a2fb05dd50
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-01 22:04:37 -0700

    DAEMON auto-committing files for Max

--- mvhfunction.m
+++ mvhfunction.m
@@ -4,11 +4,7 @@ function varargout=mvhfunction(L)
 % n: the max number of slepian functions to optimize L for
 % The Iceland.mat file I use is built out of a ShapeFile my good friend
 % Vivian Arriaga, a GIS major at ASU, made for me: varriag1@asu.edu
-np=12;
-swl=1;
-index=1;
 reg=iceland(0,1);
-fozo=6;
 
 % For iceland:
 c11=[-25.2246 66.861];
@@ -21,54 +17,10 @@ n = ((L+1)^2) * Ao4p;
 J=max(int8(n), 1);
 [G,V,~,~,N,GM2AL,MTAP,IMTAP]=glmalpha(XY,L,1,[],[],[],J,0);
 % Reorder
-[dems,dels,~,lmcosi,~,~,~,~,~,ronm]=addmon(sqrt(length(G))-1);
+[~,~,~,lmcosi,~,~,~,~,~,ronm]=addmon(sqrt(length(G))-1);
 lmcosi(2*length(lmcosi)+ronm)=G(:,1);
-data=plotplm(lmcosi,[],[],4,1);
-
-[ah,~,H]=krijetem(subnum(3,4));
-
-[~,C,~,~,~]=localization(L,reg,[],np);
-% Modify to do only partial reconstruction to save time
-r=NaN([181 361 np]);
-if L>48
-  h=waitbar(0,sprintf(...
-      'Expanding spherical harmonics up to degree %i',L));
-end
-% Get a file with the harmonics to reuse
-[r(:,:,1),~,~,Plm]=plm2xyz([dels dems C{1}],1);
-whichone=1+(index-1);
-if L>48
-    waitbar(index/12,h)
-end
-if index>1
-% In the next line should expand only in the region to save time
-r(:,:,index)=plm2xyz([dels dems C{whichone}],1,[],[],[],Plm);
-end
-ka=swl*r(:,:,index);
-% Use setnans for this, rather
-ka=ka/max(max(abs(ka)));
-ka(abs(ka)<0.01)=NaN;
-axes(ah(index))
-imagefnan([0 90-100*eps],[360 -90],ka,'kelicol',[-1 1])
-
-axis image
-set(ah,'FontSize',fozo-2)
-
-[~,pc]=plotcont;
-axis([0 360 -90 90])
-set(pc,'Linew',0.5);
-title(sprintf('%s = %.13g','\lambda',V(whichone)));
-longticks(ah)
-set(ah,'xgrid','off','ygrid','off')
-
-set(ah,'xlim',cmn)
-set(ah,'ylim',c11)
-set(ah,'XTick',cmn,'XTickLabel',cmn,...
-	 'YTick',c11,'YTickLabel',c11)
-serre(H,1/3,'across')
-serre(H',1/3,'down')
-set(ah,'camerav',8)
-
+data=plotplm(lmcosi,[],[],5,0.01);
+% PLOTPLM(lmcosi,[],[],meth,degres,th0,sres,cax)
 % Prepare outputs
 varns={G,V,data,N,GM2AL,MTAP,IMTAP,Klmlmp};
 varargout=varns(1:nargout);
