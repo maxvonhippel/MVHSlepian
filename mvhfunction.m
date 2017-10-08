96483217798f89a1d5671523691cfd2f6da0bff5
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-08 13:37:07 -0700

    Fixed the positioning problem with geoshow, so I'm not making good looking plots of Iceland on my own for Slepian fuctions.  Next I need to work with the gravity data.

--- mvhfunction.m
+++ mvhfunction.m
@@ -4,6 +4,10 @@ function varargout=mvhfunction(L, degres)
 % n: the max number of slepian functions to optimize L for
 % The Iceland.mat file I use is built out of a ShapeFile my good friend
 % Vivian Arriaga, a GIS major at ASU, made for me: varriag1@asu.edu
+
+% degres must be >= 1
+% for example, mvhfunction(4,1) should work
+
 reg=iceland(0,1);
 % For iceland:
 c11=[-25.2246 66.861];
@@ -29,15 +33,25 @@ lmcosi(2*length(lmcosi)+ronm)=G(:,1);
 % Make a nice plot
 % data=plotplm(lmcosi,[],[],5,degres);
 [r,lon,lat,Plm]=plm2xyz(lmcosi,degres);
-indeks1=repmat(lon,length(1:90),1);
-indeks2=repmat(lat(1:90)',1,length(lon));
+% plotplm(lmcosi,[],[],4,1)
+indeks1=repmat(lon,length(1:181),1);
+indeks2=repmat(lat(1:181)',1,length(lon));
 figure
+% axesm('mercator','Origin',[70 318 0],...
+%      'FLatLimit',[-7 1],...
+%      'FLonLimit',[4 14]);
 axesm('mercator','Origin',[70 318 0],...
-     'FLatLimit',[-7 1],...
-     'FLonLimit',[4 14]);
+     'FLatLimit',[-20 20],...
+     'FLonLimit',[-20 20]);
 axis off; framem on; gridm on; tightmap
-geoshow(indeks2,indeks1,lmcosi,'DisplayType','texturemap')
-geoshow(XY(:,2),XY(:,1),'DisplayType','line')
+geoshow(indeks2,indeks1,r,'DisplayType','texturemap')
+% Get border of greenland and iceland
+XY2=greenland(0,0);
+XY1=iceland(0,0);
+% Draw greenland
+geoshow(XY2(:,2),XY2(:,1),'DisplayType','line')
+% Draw iceland
+geoshow(XY1(:,2),XY1(:,1),'DisplayType','line')
 kelicol
 caxis([-max(abs(reshape(peaks,[],1))) max(abs(reshape(peaks,[],1)))]);
 colorbar
