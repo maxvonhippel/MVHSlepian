76a9ed7095bccc846b34c4e8efb0f220231da948
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-08 14:36:58 -0700

    Code is now working how it should, just need to get degres, resolution hooked up, fix some of the formatting for labels and axes, and fix the color scale, for the 9-subplot script

--- Fig_slepfuncs_1.m
+++ Fig_slepfuncs_1.m
@@ -66,7 +66,7 @@ end
 % Figure of Slepian Functions for Greenland for the chosen bandwidth
 figure
 ah1=krijetem(subnum(3,3));
-fig2print(gcf,'tall')
+fig2print(gcf,'landscape')
   
 %Main top text
 axes('position',[0,0,1,1]);  % Define axes for the text.
@@ -81,214 +81,31 @@ set(htext,'HorizontalAlignment','center');
 set(gca,'Visible','off');
 
 % The integrals here are expressed as fractional sphere area
-  
-% Panel 1
-axes(ah1(1));
+
 indeks1=repmat(lon,length(1:181),1);
 indeks2=repmat(lat(1:181)',1,length(lon));
-ah1(1)=axesm('mercator','Origin',[70 318 0],...
-     'FLatLimit',[-20 20],...
-     'FLonLimit',[-20 20]);
-geoshow(indeks2,indeks1,r{1}(1:181,:),'DisplayType','texturemap')
-% Get border of greenland and iceland
 XY2=greenland(0,0);
 XY1=iceland(0,0);
-% Draw greenland
-geoshow(XY2(:,2),XY2(:,1),'DisplayType','line')
-% Draw iceland
-geoshow(XY1(:,2),XY1(:,1),'DisplayType','line')
-% Color code, add colorbar, and set up axis
+for panel=1:9
+%   Current subplot axes
+    axes(ah1(panel));
+    ah1(panel)=axesm('mercator','Origin',[70 318 0],...
+     'FLatLimit',[-20 20],...
+     'FLonLimit',[-20 20]);
+    geoshow(indeks2,indeks1,r{panel}(1:181,:),'DisplayType','texturemap')
+    % Draw greenland
+    geoshow(XY2(:,2),XY2(:,1),'DisplayType','line')
+    % Draw iceland
+    geoshow(XY1(:,2),XY1(:,1),'DisplayType','line')
+    caxis([-max(abs(reshape(peaks,[],1))) max(abs(reshape(peaks,[],1)))]);
+    [Int,A,miniK,XY]=plm2avg(CC{panel},XY);
+    if XYbuffer ~= 0, linem(XY(:,1),XY(:,2),'color','white','linestyle','--'); end
+    t=title(['alpha=1, lambda_1 = ' num2str(V(panel),3) ', Avg Val= ' num2str(A,3)]);
+    P=get(t,'position');
+    P(2)=P(2)*1.05;
+    set(t,'position',P);
+end
+
+%   Color code everything
 kelicol
-caxis([-max(abs(reshape(peaks,[],1))) max(abs(reshape(peaks,[],1)))]);
-colorbar
-
-%t = colorbar('peer',gca);
-[Int,A,miniK,XY]=plm2avg(CC{1},XY);
-if XYbuffer ~= 0, linem(XY(:,1),XY(:,2),'color','white','linestyle','--'); end
-t=title(['alpha=1, lambda_1 = ' num2str(V(1),3) ', Avg Val= ' num2str(A,3)]);
-P=get(t,'position');
-P(2)=P(2)*1.05;
-set(t,'position',P);
-  
-% Panel 2
-axes(ah1(2));
-[Int,A,miniK,XY]=plm2avg(CC{2},XY);
-if XYbuffer ~= 0, linem(XY(:,1),XY(:,2),'color','white','linestyle','--'); end
-t=title(['alpha=2, lambda_2 = ' num2str(V(2),3) ', Avg Val= ' num2str(A,3)]);
-P=get(t,'position');
-P(2)=P(2)*1.05;
-set(t,'position',P);
-  
-% Panel 3
-axes(ah1(3));
-m_proj('oblique mercator','longitudes',[220 220],'latitudes',[75 40],'aspect',1.0);
-m_pcolor(lon,lat(1:100),r{3}(1:100,:)); % 90 to 10, resolution is 1 degree
-shading flat;
-m_grid;
-m_coast('color','w');
-%t = colorbar('peer',gca);
-[Int,A,miniK,XY]=plm2avg(CC{3},XY);
-if XYbuffer ~= 0, m_line(XY(:,1),XY(:,2),'color','white','linestyle','--'); end
-t=title(['alpha=3, lambda_3 = ' num2str(V(3),3) ', Avg Val= ' num2str(A,3)]);
-P=get(t,'position');
-P(2)=P(2)*1.05;
-set(t,'position',P);
-  
-% Panel 4
-axes(ah1(4));
-m_proj('oblique mercator','longitudes',[220 220],'latitudes',[75 40],'aspect',1.0);
-m_pcolor(lon,lat(1:100),r{4}(1:100,:)); % 90 to 10, resolution is 1 degree
-shading flat;
-m_grid;
-m_coast('color','w');
-%t = colorbar('peer',gca);
-[Int,A,miniK,XY]=plm2avg(CC{4},XY);
-if XYbuffer ~= 0, m_line(XY(:,1),XY(:,2),'color','white','linestyle','--'); end
-t=title(['alpha=4, lambda_4 = ' num2str(V(4),3) ', Avg Val= ' num2str(A,3)]);
-P=get(t,'position');
-P(2)=P(2)*1.05;
-set(t,'position',P);
-  
-% Panel 5
-axes(ah1(5));
-m_proj('oblique mercator','longitudes',[220 220],'latitudes',[75 40],'aspect',1.0);
-m_pcolor(lon,lat(1:100),r{5}(1:100,:)); % 90 to 10, resolution is 1 degree
-shading flat;
-m_grid;
-m_coast('color','w');
-%t = colorbar('peer',gca);
-[Int,A,miniK,XY]=plm2avg(CC{5},XY);
-if XYbuffer ~= 0, m_line(XY(:,1),XY(:,2),'color','white','linestyle','--'); end
-t=title(['alpha=5, lambda_5 = ' num2str(V(5),3) ', Avg Val= ' num2str(A,3)]);
-P=get(t,'position');
-P(2)=P(2)*1.05;
-set(t,'position',P);
-  
-% Panel 6
-axes(ah1(6));
-m_proj('oblique mercator','longitudes',[220 220],'latitudes',[75 40],'aspect',1.0);
-m_pcolor(lon,lat(1:100),r{6}(1:100,:)); % 90 to 10, resolution is 1 degree
-shading flat;
-m_grid;
-m_coast('color','w');
-%t = colorbar('peer',gca);
-[Int,A,miniK,XY]=plm2avg(CC{6},XY);
-if XYbuffer ~= 0, m_line(XY(:,1),XY(:,2),'color','white','linestyle','--'); end
-t=title(['alpha=6, lambda_6 = ' num2str(V(6),3) ', Avg Val= ' num2str(A,3)]);
-P=get(t,'position');
-P(2)=P(2)*1.05;
-set(t,'position',P);
-
-% Panel 7
-axes(ah1(7));
-m_proj('oblique mercator','longitudes',[220 220],'latitudes',[75 40],'aspect',1.0);
-m_pcolor(lon,lat(1:100),r{7}(1:100,:)); % 90 to 10, resolution is 1 degree
-shading flat;
-m_grid;
-m_coast('color','w');
-%t = colorbar('peer',gca);
-[Int,A,miniK,XY]=plm2avg(CC{7},XY);
-if XYbuffer ~= 0, m_line(XY(:,1),XY(:,2),'color','white','linestyle','--'); end
-t=title(['alpha=7, lambda_7 = ' num2str(V(7),3) ', Avg Val= ' num2str(A,3)]);
-P=get(t,'position');
-P(2)=P(2)*1.05;
-set(t,'position',P);
-  
-% Panel 8
-axes(ah1(8));
-m_proj('oblique mercator','longitudes',[220 220],'latitudes',[75 40],'aspect',1.0);
-m_pcolor(lon,lat(1:100),r{8}(1:100,:)); % 90 to 10, resolution is 1 degree
-shading flat;
-m_grid;
-m_coast('color','w');
-%t = colorbar('peer',gca);
-[Int,A,miniK,XY]=plm2avg(CC{8},XY);
-if XYbuffer ~= 0, m_line(XY(:,1),XY(:,2),'color','white','linestyle','--'); end
-t=title(['alpha=8, lambda_8 = ' num2str(V(8),3) ', Avg Val= ' num2str(A,3)]);
-P=get(t,'position');
-P(2)=P(2)*1.05;
-set(t,'position',P);
-  
-% Panel 9
-axes(ah1(9));
-m_proj('oblique mercator','longitudes',[220 220],'latitudes',[75 40],'aspect',1.0);
-m_pcolor(lon,lat(1:100),r{9}(1:100,:)); % 90 to 10, resolution is 1 degree
-shading flat;
-m_grid;
-m_coast('color','w');
-%t = colorbar('peer',gca);
-[Int,A,miniK,XY]=plm2avg(CC{9},XY);
-if XYbuffer ~= 0, m_line(XY(:,1),XY(:,2),'color','white','linestyle','--'); end
-t=title(['alpha=9, lambda_9 = ' num2str(V(6),3) ', Avg Val= ' num2str(A,3)]);
-P=get(t,'position');
-P(2)=P(2)*1.05;
-set(t,'position',P);
-  
-  keyboard
-
-  
-  
-%%%
-% OUTPUT
-%%%
-
-% Save relevant data for plotting in something like GMT
-
-if strcmp(dom,'greenland') || strcmp(dom,'gulfofalaskaN')...
-        || strcmp(dom,'gulfofalaskaS') || strcmp(dom,'ellesmere')...
-        || strcmp(dom,'baffin')
-    mylimit = [1 71];
-    XY=eval(sprintf('%s(%i,%f)',TH{1},10,TH{2}));
-elseif strcmp(dom,'wantarctica')...
-        || strcmp(dom,'wantarcticaG') || strcmp(dom,'wantarcticaCoasts')...
-        || strcmp(dom,'pantarctica') || strcmp(dom,'pantarcticaOceanBuf')...
-        || strcmp(dom,'wantarcticaGOceanBuf')
-    mylimit = [131 181];
-    XY=eval(sprintf('%s(%i,%f)',TH{1},10,TH{2}));
-elseif strcmp(dom,'eantarcticaCoasts1') || strcmp(dom,'eantarcticaInt')...
-        || strcmp(dom,'eantarcticaCoasts2') || strcmp(dom,'eantarcticaIntGOceanBuf')...
-        || strcmp(dom,'eantarcticaCoasts1OceanBuf') || strcmp(dom,'eantarcticaCoasts2OceanBuf')...
-        || strcmp(dom,'antarcticaGP') || strcmp(dom,'antarctica')
-    mylimit = [131 181];
-    XY=eval(sprintf('%s(%i,%f)',TH{1},10,TH{2}));
-    [thetap,phip,rotmats]=rottp((90-XY(:,2))*pi/180,XY(:,1)/180*pi,0,-pi/2,0);
-    XY = [phip*180/pi 90-thetap*180/pi];
-end
-
-% Clip the data to our limit, and scale it by its maximum
-for temp = 1:K
-    mydata1{temp} = r{temp}(mylimit(1):mylimit(2),:)/max(max(abs(r{temp})));
-    mydata1{temp}(abs(mydata1{temp}(:))<0.01)=0;
-end
-
-[m,n]=size(mydata1{1});
-indeks1 = repmat(lon,length(mylimit(1):mylimit(2)),1);
-indeks2 = repmat(lat(mylimit(1):mylimit(2))',1,length(lon));
-
-indeks1 = reshape(indeks1,m*n,1);
-indeks2 = reshape(indeks2,m*n,1);
-
-fp1 = fopen(['figures/figdata/SlepEigfuncs' dom num2str(XYbuffer) num2str(L) '.dat'],'wt');
-fp2 = fopen(['figures/figdata/SlepFuncs_region_' dom num2str(XYbuffer) '.dat'],'wt');
-
-fprintf(fp2,'%.4e %.4e \n',XY');
-
-for temp1=1:(m*n)
-    if temp1==1
-        fprintf(fp1,'%i %.1f \n',L,XYbuffer);
-        fprintf(fp1,'%s \n',num2str(V(1:K),3)); % the lambdas
-    end    
-    tosave1=[];
-    formatstring1=['%i %i']; % Lon Lat
-    for temp2=1:K
-      tosave1 = [tosave1 mydata1{temp2}(temp1)];
-      formatstring1 = [formatstring1 ' %.4e'];
-    end
-    formatstring1 = [formatstring1 '\n'];
-    %tosave1 = [functimeseries(1,temp1) alphavar(temp1)];
-    fprintf(fp1,formatstring1,indeks1(temp1),indeks2(temp1),tosave1);
-end
-fclose(fp1);
-fclose(fp2);
-
-  
+colorbar 
