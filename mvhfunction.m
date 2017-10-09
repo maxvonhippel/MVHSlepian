4e8b3da5b88e0fe4c7a36b77643c8e4daddfcb14
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-08 22:43:48 -0700

    Major progress on 9-plot figures formatting

--- mvhfunction.m
+++ mvhfunction.m
@@ -1,12 +1,9 @@
-function varargout=mvhfunction(L, degres)
+function varargout=mvhfunction(L)
 % Last modified by maxvonhippel-at-email.arizona.edu, 09/27/2017
-% regn: the name of the region (eg, 'greenland')
-% n: the max number of slepian functions to optimize L for
+% L: the bandwidth to count up to in the LWindow
 % The Iceland.mat file I use is built out of a ShapeFile my good friend
 % Vivian Arriaga, a GIS major at ASU, made for me: varriag1@asu.edu
 
-% degres must be >= 1
-% for example, mvhfunction(4,1) should work
 defval('L',60);
 defval('TH',{'iceland' 0.5});
 defval('pars',10);
@@ -23,7 +20,7 @@ eval(sprintf('XY=%s(10,%f);',dom,XYbuffer));
 [~,~,~,lmcosiW,~,~,~,~,~,ronmW]=addmon(L);
 % Get the Slepian basis; definitely not block-sorted as for the rotated
 % versions this will make no sense at all anymore
-[G,V,EL,EM,N,GM2AL,MTAP,IMTAP]=glmalpha(TH,L,[],0,[],[],J);
+[G,V,~,~,~,~,MTAP,~]=glmalpha(TH,L,[],0,[],[],J);
 % Sort by decreasing eigenvalue
 [V,vi]=sort(V,'descend');
 G=G(:,vi); if ~isnan(MTAP); MTAP=MTAP(vi); end
@@ -37,31 +34,25 @@ for j=1:size(G,2)
    CC{j} = [lmcosiW(:,1:2) cosi]; 
 end
 % Expand eigenfunctions into space
-for i=1:K
-    [r{i},lon,lat]=plm2xyz(CC{i},1);
-end
+for i=1:K; [r{i},lon,lat]=plm2xyz(CC{i}, 1); end
 [dems,dels,mz,lmc,mzin]=addmon(L);
-%%%
-% PLOTTING
-%%%
+
 figure
 ah1=krijetem(subnum(3,3));
-fig2print(gcf,'landscape')
+fig2print(gcf, 'landscape');
   
-%Main top text
-axes('position',[0,0,1,1]);  % Define axes for the text.
-% In this case, the axes are for the entire page.
+% Define axes for the main top text.
+axes('position', [0, 0, 1, 1]);
 % Write some text: 
-htext = text(.5,0.98,['Functions from PLM2SLEP:  dom = '...
+htext = text(.5, 0.98, ['Functions from PLM2SLEP:  dom = '...
     num2str(dom) '+' num2str(XYbuffer) 'buffer, Lwindow = '...
-    num2str(L)], 'FontSize', 12  );
+    num2str(L)], 'FontSize', 14, 'FontName', 'Times New Roman');
 % Specify that the coordinates provided above are for the middle
 % of the text string: 
-set(htext,'HorizontalAlignment','center');
-set(gca,'Visible','off');
+set(htext, 'HorizontalAlignment', 'center');
+set(gca, 'Visible', 'off');
 
-% The integrals here are expressed as fractional sphere area
-
+% Set up the 9 individual plots
 indeks1=repmat(lon,length(1:181),1);
 indeks2=repmat(lat(1:181)',1,length(lon));
 XY2=greenland(0,0);
@@ -69,33 +60,43 @@ XY1=iceland(0,0);
 for panel=1:9
 %   Current subplot axes
     axes(ah1(panel));
-    ah1(panel)=axesm('mercator','Origin',[70 318 0],...
-     'FLatLimit',[-20 20],...
-     'FLonLimit',[-20 20]);
-    geoshow(indeks2,indeks1,r{panel}(1:181,:),'DisplayType','texturemap')
-    % Draw greenland
+    ah1(panel)=axesm('mercator',...
+                     'Origin', [70 318 0],...
+                     'FLatLimit',[-20 20],...
+                     'FLonLimit',[-20 20]);
+    geoshow(indeks2, indeks1, r{panel}(1:181,:),...
+            'DisplayType', 'texturemap')
+%   Draw greenland, then iceland
     geoshow(XY2(:,2),XY2(:,1),'DisplayType','line')
-    % Draw iceland
     geoshow(XY1(:,2),XY1(:,1),'DisplayType','line')
-    caxis([-max(abs(reshape(peaks,[],1))) max(abs(reshape(peaks,[],1)))]);
-    [Int,A,miniK,XY]=plm2avg(CC{panel},XY);
-    if XYbuffer ~= 0, linem(XY(:,1),XY(:,2),...
-            'color','white','linestyle','--'); end
-    t=title(['alpha=1, lambda_1 = ' num2str(V(panel),3)...
-        ', Avg Val= ' num2str(A,3)]);
-    P=get(t,'position');
-    P(2)=P(2)*1.05;
-    set(t,'position',P);
+%   Shape axes
+    caxis([-max(abs(reshape(peaks, [], 1)))...
+            max(abs(reshape(peaks, [], 1)))]);
+    [~,A,~,XY]=plm2avg(CC{panel}, XY);
+    if XYbuffer ~= 0, linem(XY(:,1) ,XY(:,2),...
+            'color', 'white', 'linestyle', '--'); end
+    t=title(['$a$ = ' num2str(panel)...
+             ', $l_{1}$ = ' num2str(V(panel),3) newline...
+             'Avg = ' num2str(A,3)],...
+             'FontName', 'Times New Roman',...
+             'FontSize', 10,...
+             'Interpreter', 'LaTeX');
+    P = get(t, 'position');
+    P(2) = (P(2)*1.05) - 0.02;
+    set(t, 'position', P);
+    pos = get(gca, 'Position');
+    pos(2) = pos(2) - 0.05;
+    set(gca, 'Position', pos)
 end
 
 kelicol
-colorbar('location','Manual', 'position', [0.93 0.1 0.02 0.81]);
+colorbar('location', 'Manual', 'position', [0.93 0.1 0.02 0.81]);
 
 % Now get the monthly grace data
-[potcoffs,cal_errors,thedates]=grace2plmt('CSR','RL05','POT',0);
+[potcoffs,cal_errors,thedates]=grace2plmt('CSR', 'RL05', 'POT', 0);
 % Project it into Slepians
 [slepcoffs,calerrors,thedates,TH,G,CC,V,N]=grace2slept(...
-    'CSRRL05','iceland',1,L,0,0,0,J,'POT',1);
+    'CSRRL05', 'iceland', 1, L, 0, 0, 0, J, 'POT', 1);
 
 % Prepare outputs
 varns={G,V,lmcosiW,dems,dels,mz,lmc,mzin};
