8fbb827573d579cf790f5c9704706d3c921fad02
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-17 13:46:19 -0700

    Improved style throughout and working on making nice plots of month diffs to compare to from grace data, almost done with that

--- mvhfunction.m
+++ mvhfunction.m
@@ -25,9 +25,9 @@ eval(sprintf('XY = %s(10, %f);', dom, XYbuffer));
 
 % ------------------------ SLEPIAN FUNCTIONS ------------------------ 
 
-% lmcosiW: Matrix with zeroes where cos/sine coefficients will go.
-% ronmW: Running index into lmcosi(3:4) unwrapping the orders.
-[~, ~, ~, lmcosiW, ~, ~, ~, ~, ~, ronmW] = addmon(L);
+% ronmosiW: Matrix with zeroes where cos/sine coefficients will go.
+% ronmW: Running index into ronmosi(3:4) unwrapping the orders.
+[dems, dels, mz, ronmosiW, ~, ~, ~, ~, ronm, mzin] = addmon(L);
 % Get the spherical harmonic matrix for the region.
 [G, V, ~, ~, ~, ~, MTAP, ~] = glmalpha(TH, L, [], 0, [], [], J);
 % Sort the spherical harmonic matrix by decreasing eigenvalue.
@@ -36,15 +36,14 @@ G = G(:, vi); if ~isnan(MTAP); MTAP = MTAP(vi); end
 % Collect the eigenvector output into a format that PLM2XYZ interprets.
 for j = 1:size(G, 2)
    % Create the blanks.
-   cosi = lmcosiW(:, 3:4);
+   cosi = ronmosiW(:, 3:4);
    % Stick in the coefficients of the 1st eigentaper.
-   cosi(ronmW) = G(:, j);
+   cosi(ronm) = G(:, j);
    % Construct the full matrix.
-   CC{j} = [lmcosiW(:,1:2) cosi]; 
+   CC{j} = [ronmosiW(:,1:2) cosi]; 
 end
 % Expand eigenfunctions into space.
 for i=1:J; [r{i}, lon, lat] = plm2xyz(CC{i}, 1); end
-[~, ~, ~, lmc, mzin] = addmon(L);
 
 % ---------------------- CHART SLEPIAN FUNCTIONS ---------------------- 
 
@@ -72,38 +71,36 @@ set(gca, 'Visible', 'off');
 indeks1 = repmat(lon, length(1:181), 1);
 indeks2 = repmat(lat(1:181)', 1, length(lon));
 % Next, the borders of Greenland and Iceland, which we shown in our figure.
-greenlandBorder = greenland(0,0);
-icelandBorder = iceland(0,0);
+greenlandBorder = greenland(0, 0);
+icelandBorder = iceland(0, 0);
 % Now we iterate over and draw the 9 different figures.
-for panel=1:9
+for panel = 1:9
 %   Select and scale the current axis for this subplot.
     axes(ah1(panel));
-    ah1(panel)=axesm('mercator',...
-                     'Origin', [70 318 0],...
-                     'FLatLimit',[-20 20],...
-                     'FLonLimit',[-20 20]);
+    ah1(panel) = axesm('mercator', 'Origin', [70 318 0], ...
+                      'FLatLimit', [-20 20], 'FLonLimit', [-20 20]);
 %   Shape axes
-    caxis([-max(abs(reshape(peaks, [], 1)))...
+    caxis([-max(abs(reshape(peaks, [], 1))) ...
             max(abs(reshape(peaks, [], 1)))]);
-    geoshow(indeks2, indeks1, r{panel}(1:181,:),...
+    geoshow(indeks2, indeks1, r{panel}(1:181, :), ...
             'DisplayType', 'texturemap')
 %   Draw Greenland & Iceland borders.
-    geoshow(greenlandBorder(:,2), greenlandBorder(:,1), ...
+    geoshow(greenlandBorder(:, 2), greenlandBorder(:, 1), ...
             'DisplayType', 'line')
-    geoshow(icelandBorder(:,2), icelandBorder(:,1), ...
+    geoshow(icelandBorder(:, 2), icelandBorder(:,1), ...
             'DisplayType', 'line')
     [~, A ,~, XY] = plm2avg(CC{panel}, XY);
-    if XYbuffer ~= 0, linem(XY(:,1) ,XY(:,2), ...
+    if XYbuffer ~= 0, linem(XY(:, 1), XY(:, 2), ...
             'color', 'white', 'linestyle', '--'); end
 %   Write the title to all the figures.
     t = title(['$a$ = ' num2str(panel) ...
-               ', $l_{1}$ = ' num2str(V(panel),3) newline ...
-               'Avg = ' num2str(A,3)],...
+               ', $l_{1}$ = ' num2str(V(panel), 3) newline ...
+               'Avg = ' num2str(A, 3)],...
               'FontName', 'Times New Roman',...
               'FontSize', 10, 'Interpreter', 'LaTeX');
 %   Rotate/reshape plot - I think this is to account for projection?
     P = get(t, 'position');
-    P(2) = (P(2)*1.05) - 0.02;
+    P(2) = (P(2) * 1.05) - 0.02;
     set(t, 'position', P);
     pos = get(gca, 'Position');
     pos(2) = pos(2) - 0.05;
@@ -118,66 +115,82 @@ colorbar('location', 'Manual', 'position', [0.93 0.1 0.02 0.81]);
 % ---------------------------- GRACE Data ----------------------------
 
 % Now get the monthly grace data
-% [potcoffs,cal_errors,thedates]=grace2plmt('CSR', 'RL05', 'POT', 0);
-% Project it into Slepians
-[slept,cal_errors,thedates,TH,G,CC,V,N]=grace2slept(...
+[potcoffs, cal_errors, thedates]=grace2plmt('CSR', 'RL05', 'POT', 0);
+figure
+% As a demo, let's show January
+axesm('mercator', 'Origin', [70 318 0], 'FLatLimit', [-20 20], ...
+       'FLonLimit',[-20 20]);
+potcoffs1 = squeeze(potcoffs(1, :, 1:4));
+potcoffs2 = squeeze(potcoffs(2, :, 1:4));
+plotcoffsDiff = potcoffs1 - potcoffs2;
+plotcoffsDiff = horzcat(potcoffs1(:, 1:2), plotcoffsDiff(:, 3:4));
+% plotplm(plotcoffsDiff, [], [], 1, 1, [], [], []);
+indeksPlotcoffs1 = repmat(lon, length(1:1890), 1);
+indeksPlotcoffs2 = repmat(lat(1:1890)', 1, length(lon));
+geoshow(indeksPlotcoffs2, indeksPlotcoffs1, plotcoffsDiff, 'DisplayType', 'texturemap');  
+
+% Project monthly GRACE data into Slepians.
+[slept, ~, thedates, TH, G, CC, V, ~] = grace2slept(...
     'CSRRL05', 'iceland', 1, L, 0, 0, 0, J, 'POT', 1);
-[dems,dels,mz,lmcosi,mzi,mzo,bigm,bigl,rinm,ronm,demin] = addmon(L);
 defval('nmonths',length(thedates));
-% Next run SLEPT2RESID to fit my slepian functions
+% Get a shannon number.
 N=max(round((L+1)^2*spharea(TH)),1);
-[ESTsignal,ESTresid,ftests,extravalues,total,alphavarall,totalparams, ...
-     totalparamerrors,totalfit,functionintegrals,alphavar,fgls_coeff,...
-     fgls_2sigma,fgls_pred,fgls_residvar]...
-        =slept2resid_fgls(slept,thedates,[3 365.0 182.5],...
-        [],[],CC(1:round(N)),TH);
-% Because acceleration is twice the coefficient value
-fgls_coeff(2) = fgls_coeff(2)*2;
-fgls_2sigma(2) = fgls_2sigma(2)*2;
-totalparams(2,:) = totalparams(2,:)*365;
-% The acceleration is 2*c2
-totalparams(3,:) = 2*totalparams(3,:)*365*365;
-totalparamerrors(2,:) = totalparamerrors(2,:)*365;
-% Acceleration error is 2*c2error
-totalparamerrors(3,:) = 2*totalparamerrors(3,:)*365*365;
+
+% Next run SLEPT2RESID to fit my slepian functions
+[ESTsignal, ESTresid, ftests, extravalues, total, alphavarall, ...
+ totalparams, totalparamerrors, totalfit, functionintegrals, alphavar, ...
+ fgls_coeff, fgls_2sigma, fgls_pred, fgls_residvar] = ...
+    slept2resid_fgls(slept, thedates, [3 365.0 182.5], [], [], ...
+                     CC(1:round(N)), TH);
+
+% Because acceleration is twice the coefficient value:
+fgls_coeff(2) = fgls_coeff(2) * 2;
+fgls_2sigma(2) = fgls_2sigma(2) * 2;
+totalparams(2, :) = totalparams(2,:) * 365;
+
+% The acceleration is 2 * c2.
+totalparams(3, :) = 2 * totalparams(3, :) * 365 * 365;
+totalparamerrors(2, :) = totalparamerrors(2, :) * 365;
+% Acceleration error is 2 * c2error.
+totalparamerrors(3, :) = 2 * totalparamerrors(3, :) * 365 * 365;
 
 figure
-errorbar(thedates,total,ones(1,length(thedates))*sqrt(alphavarall)*1.99,'k-')
+errorbar(thedates, total, ...
+         ones(1, length(thedates)) * sqrt(alphavarall) * 1.99, 'k-');
 ylim([-500 500]);
 hold on
-plot(totalfit(:,1),totalfit(:,3),'b-',totalfit(:,1),totalfit(:,3)+totalfit(:,5),'b--',...
-    totalfit(:,1),totalfit(:,3)-totalfit(:,5),'b--');
-datetick('x',28);
-text(datenum('01-Jan-2003'),-1500,...
-    ['Slope = ' num2str(totalparams(2,2)) ' +- ' num2str(totalparamerrors(2,2))...
-    ' Gt/yr']);
-text(datenum('01-Jan-2003'),-1800,...
-    ['Acceleration = ' num2str(totalparams(3,2)) ' +- ' num2str(totalparamerrors(3,2))...
-    ' Gt/yr']);
+plot(totalfit(:, 1), totalfit(:, 3), 'b-', totalfit(:, 1), ...
+    totalfit(:, 3) + totalfit(:, 5), 'b--', ...
+    totalfit(:, 1), totalfit(:, 3) -totalfit(:, 5), 'b--');
+datetick('x', 28);
+text(datenum('01-Jan-2003'), -1500, ...
+    ['Slope = ' num2str(totalparams(2, 2)) ' +- ' ...
+    num2str(totalparamerrors(2, 2)) ' Gt/yr']);
+text(datenum('01-Jan-2003'), -1800, ...
+    ['Acceleration = ' num2str(totalparams(3, 2)) ' +- ' ...
+    num2str(totalparamerrors(3, 2)) ' Gt/yr']);
 ylabel('Mass (Gt)');
-title(['Integrated Mass Change for Greenland, L = ' num2str(L) ...
+title(['Integrated Mass Change for Iceland, L = ' num2str(L) ...
     ', buffer = ' num2str(XYbuffer) ' deg']);
 
 
 figure
-errorbar(thedates,total,ones(1,length(thedates))*sqrt(fgls_residvar)*1.99,'k-');
+errorbar(thedates, total, ...
+         ones(1, length(thedates)) * sqrt(fgls_residvar) * 1.99, 'k-');
 ylim([-500 500]);
 hold on
 plot(totalfit(:,1),fgls_pred,'b-');
 datetick('x',28);
-text(datenum('01-Jan-2003'),-1500,...
-    ['Slope = ' num2str(fgls_coeff(1)) ' +- ' num2str(fgls_2sigma(1))...
+text(datenum('01-Jan-2003'), -1500, ...
+    ['Slope = ' num2str(fgls_coeff(1)) ' +- ' num2str(fgls_2sigma(1)) ...
     ' Gt/yr']);
-text(datenum('01-Jan-2003'),-1800,...
-    ['Acceleration = ' num2str(fgls_coeff(2)) ' +- ' num2str(fgls_2sigma(2))...
-    ' Gt/yr']);
+text(datenum('01-Jan-2003'), -1800, ...
+    ['Acceleration = ' num2str(fgls_coeff(2)) ' +- ' ...
+     num2str(fgls_2sigma(2)) ' Gt/yr']);
 ylabel('Mass (Gt)');
 title(['Integrated Mass Change (FGLS), L = ' num2str(L) ...
-    ', buffer = ' num2str(XYbuffer) ' deg']);
-% figure
-% axesm('mercator','Origin', [70 318 0],'FLatLimit',[-20 20],...
-%                  'FLonLimit',[-20 20]);
-% geoshow(indeks2, indeks1, ESTsignal,'DisplayType', 'texturemap')              
+       ', buffer = ' num2str(XYbuffer) ' deg']);            
+
 % Prepare outputs
-varns={G,V,lmcosiW,dems,dels,mz,lmc,mzin};
+varns={G,V,ronmosiW,dems,dels,mz,ronm,mzin};
 varargout=varns(1:nargout);
