b22c869e9075ede94b932967d035ee7b2f4d76a7
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-19 16:56:28 -0700

    DAEMON auto-committing files for Max

--- mvhfunction.m
+++ mvhfunction.m
@@ -5,10 +5,16 @@ function varargout = mvhfunction(L)
 %           SDWREGIONS, LOCALIZATION, PLM2XYZ, REGIONS/ICELAND,
 %           HARIGIT/HS12TOTALTREND
 % 
-% Last modified by maxvonhippel-at-email.arizona.edu, 10/16/2017
+% Last modified by maxvonhippel-at-email.arizona.edu, 10/19/2017
 % The Iceland.mat file is based on a ShapeFile by varriag1-at-asu.edu
 
 % ------------------------ DEFAULT VALUES ------------------------ 
+% Note that while these are mostly not currently things that can
+% be set via any argument, exposing them here a) makes it clear
+% that down the line the code can be modified to make them available
+% as arguments, and b) makes it easier to play around with different
+% defaults throughout the script.
+fprintf('\nSetting default values (Step 1/6)\n');
 
 % Maximum bandwidth for LWindow.
 defval('L', 60);
@@ -22,8 +28,29 @@ defval('XYbuffer', TH{2});
 defval('dom', TH{1});
 % Initialize XY to point to the domain, buffered.
 eval(sprintf('XY = %s(10, %f);', dom, XYbuffer));
+% Define IFILES
+IFILES=getenv('IFILES');
+% Which center do we get our data from?
+defval('Pcenter', 'CSR');
+% Used to calculate unit signal per month
+defval('Signal', 200); % Gt/yr
+% A boolean representing weather or not we want noise
+defval('wantNoise', 0);
+% L values we iterate over? Not sure I fully understand this variable rn
+defval('Ls', [45 50 55]);
+% Distances in degrees that the region outline will be enlarged by
+defval('buffers', [0 1 2]);
+% The number of functions that we want to vary away from the Shannon number,
+% e.g. [-2 -1 0 1 2].  The Shannon number will be added to this array to 
+% end up with something like [N-2 N-1 N N+1 N+2].
+defval('truncations', [-2 -1 0 1 2]);
 
-% ------------------------ SLEPIAN FUNCTIONS ------------------------ 
+% Run in parallel if possible
+delete(gcp('nocreate'));
+parpool;
+
+% ---------------- CALCULATE SLEPIAN FUNCTIONS ------------------ 
+fprintf('\nCalculating Slepian Functions (Step 2/6)\n');
 
 % ronmosiW: Matrix with zeroes where cos/sine coefficients will go.
 % ronmW: Running index into ronmosi(3:4) unwrapping the orders.
@@ -46,6 +73,7 @@ end
 for i=1:J; [r{i}, lon, lat] = plm2xyz(CC{i}, 1); end
 
 % ---------------------- CHART SLEPIAN FUNCTIONS ---------------------- 
+fprintf('\nSharting Slepian Functions (Step 3/6)\n');
 
 figure
 % Make 9 subplots which we will populare with bandwidth values along
@@ -83,12 +111,12 @@ for panel = 1:9
     caxis([-max(abs(reshape(peaks, [], 1))) ...
             max(abs(reshape(peaks, [], 1)))]);
     geoshow(indeks2, indeks1, r{panel}(1:181, :), ...
-            'DisplayType', 'texturemap')
+            'DisplayType', 'texturemap');
 %   Draw Greenland & Iceland borders.
     geoshow(greenlandBorder(:, 2), greenlandBorder(:, 1), ...
-            'DisplayType', 'line')
+            'DisplayType', 'line');
     geoshow(icelandBorder(:, 2), icelandBorder(:,1), ...
-            'DisplayType', 'line')
+            'DisplayType', 'line');
     [~, A ,~, XY] = plm2avg(CC{panel}, XY);
     if XYbuffer ~= 0, linem(XY(:, 1), XY(:, 2), ...
             'color', 'white', 'linestyle', '--'); end
@@ -112,36 +140,36 @@ end
 kelicol
 colorbar('location', 'Manual', 'position', [0.93 0.1 0.02 0.81]);
 
-% ---------------------------- GRACE Data ----------------------------
+% ----------------------- Get, Process GRACE Data ---------------------
+fprintf('\nGetting & Processing GRACE Data (Step 4/6)\n');
 
 % Now get the monthly grace data
-[potcoffs, cal_errors, thedates]=grace2plmt('CSR', 'RL05', 'POT', 0);
-figure
-% As a demo, let's show January
-axesm('mercator', 'Origin', [70 318 0], 'FLatLimit', [-20 20], ...
-       'FLonLimit',[-20 20]);
-potcoffs1 = squeeze(potcoffs(1, :, 1:4));
-potcoffs2 = squeeze(potcoffs(2, :, 1:4));
-plotcoffsDiff = potcoffs1 - potcoffs2;
-plotcoffsDiff = horzcat(potcoffs1(:, 1:2), plotcoffsDiff(:, 3:4));
-% plotplm(plotcoffsDiff, [], [], 1, 1, [], [], []);
-indeksPlotcoffs1 = repmat(lon, length(1:1890), 1);
-indeksPlotcoffs2 = repmat(lat(1:1890)', 1, length(lon));
-geoshow(indeksPlotcoffs2, indeksPlotcoffs1, plotcoffsDiff, 'DisplayType', 'texturemap');  
+[potcoffs, cal_errors, thedates] = grace2plmt(Pcenter, 'RL05', 'POT', 0);  
+
+% Number of months our data looks at
+defval('nmonths', length(thedates));
 
 % Project monthly GRACE data into Slepians.
-[slept, ~, thedates, TH, G, CC, V, ~] = grace2slept(...
+[slept, ~, thedates, TH, G, CC, V, ~] = grace2slept( ...
     'CSRRL05', 'iceland', 1, L, 0, 0, 0, J, 'POT', 1);
 defval('nmonths',length(thedates));
+
 % Get a shannon number.
 N=max(round((L+1)^2*spharea(TH)),1);
 
 % Next run SLEPT2RESID to fit my slepian functions
+% This step doesn't seem to work well for L < 60, and frankly, 
+% I have not yet really confidently figured out steps 4 and 5
+% of this script.  I am confident that the charts I draw in
+% step 5, when I do draw them, are wrong, though.
 [ESTsignal, ESTresid, ftests, extravalues, total, alphavarall, ...
  totalparams, totalparamerrors, totalfit, functionintegrals, alphavar, ...
  fgls_coeff, fgls_2sigma, fgls_pred, fgls_residvar] = ...
     slept2resid_fgls(slept, thedates, [3 365.0 182.5], [], [], ...
                      CC(1:round(N)), TH);
+
+% -------------------------- Chart GRACE Data ------------------------
+fprintf('\nCharting GRACE Data (Step 5/6)\n');
 
 % Because acceleration is twice the coefficient value:
 fgls_coeff(2) = fgls_coeff(2) * 2;
@@ -154,10 +182,24 @@ totalparamerrors(2, :) = totalparamerrors(2, :) * 365;
 % Acceleration error is 2 * c2error.
 totalparamerrors(3, :) = 2 * totalparamerrors(3, :) * 365 * 365;
 
+% figure
+% As a demo, let's show January
+
+% axesm('mercator', 'Origin', [70 318 0], 'FLatLimit', [-20 20], ...
+%        'FLonLimit',[-20 20]);
+% potcoffs1 = squeeze(potcoffs(1, :, 1:4));
+% potcoffs2 = squeeze(potcoffs(2, :, 1:4));
+% plotcoffsDiff = potcoffs1 - potcoffs2;
+% plotcoffsDiff = horzcat(potcoffs1(:, 1:2), plotcoffsDiff(:, 3:4));
+% plotplm(plotcoffsDiff, [], [], 1, 1, [], [], []);
+
+% geoshow(indeks2, indeks1, plotcoffsDiff(1:181, :), ...
+%         'DisplayType', 'texturemap');
+
 figure
 errorbar(thedates, total, ...
          ones(1, length(thedates)) * sqrt(alphavarall) * 1.99, 'k-');
-ylim([-500 500]);
+ylim([-1 1]);
 hold on
 plot(totalfit(:, 1), totalfit(:, 3), 'b-', totalfit(:, 1), ...
     totalfit(:, 3) + totalfit(:, 5), 'b--', ...
@@ -177,7 +219,7 @@ title(['Integrated Mass Change for Iceland, L = ' num2str(L) ...
 figure
 errorbar(thedates, total, ...
          ones(1, length(thedates)) * sqrt(fgls_residvar) * 1.99, 'k-');
-ylim([-500 500]);
+ylim([-1 1]);
 hold on
 plot(totalfit(:,1),fgls_pred,'b-');
 datetick('x',28);
@@ -190,6 +232,60 @@ text(datenum('01-Jan-2003'), -1800, ...
 ylabel('Mass (Gt)');
 title(['Integrated Mass Change (FGLS), L = ' num2str(L) ...
        ', buffer = ' num2str(XYbuffer) ' deg']);            
+
+% ----------------------- Synthetic Experiment A ----------------------
+fprintf('\nGetting & Processing GRACE Data (Step 6/6)\n');
+
+% Find the noise
+[ESTresid] = plmt2resid(potcoffs, thedates, [1 1 365.0], cal_errors);
+% Find the noise covariance
+% Currently broken for some reason, returning:
+% NaNs, 0s, NaNs, EL and EM look OK though
+[Clmlmp, Clmlmpr, Clmlmpd, EL, EM] = plmresid2cov(ESTresid, L, []);
+tic;
+% Decompose the covariance matrix
+disp('Decomposing the covariance...')
+T = cholcov(Clmlmp);
+[n, m] = size(T);
+% Make a synthetic unit signal over Greenland
+[~,~,~,~,~,lmcosiS] = geoboxcap(120, dom, [], [], 1);
+% Convert desired Gt/yr to kg
+factor1 = Signal * 907.1847 * 10^9;
+% Then get an average needed for greenland (area in meters)
+factor1 = factor1 / spharea(dom) / 4 / pi / 6370000^2;
+% So now we have kg/m^2
+% Get relative dates to make a trend
+deltadates = thedates - thedates(1);
+% Preallocate
+lmcosiSSD = zeros(length(thedates), size(lmcosiS, 1), size(lmcosiS, 2));
+fullS = zeros(length(thedates), size(lmcosiS, 1), size(lmcosiS, 2));
+counter = 1;
+
+for k = deltadates
+    % Caltulate the desired trend amount for this month, putting the mean
+    % approximately in the middle (4th year)
+    factor2 = factor1 * 4 - k / 365 * factor1;
+    % Scale the unit signal for this month
+    lmcosiSSD(counter, :, :) = [lmcosiS(:, 1:2) lmcosiS(:, 3:4) * factor2];
+    % Make a synthetic noise realization
+    syntheticnoise = randn(1, n) * T;
+    % Reorder the noise
+    temp1 = ronmosiW(:, 3:4);
+    temp1(ronm) = syntheticnoise(:);
+    syntheticnoise = [ronmosiW(:, 1:2) temp1];
+    % Add this to the signal
+    if wantnoise
+       fullS(counter, :, :) = [ronmosiW(:, 1:2)...
+           squeeze(lmcosiSSD(counter, :, 3:4)) + syntheticnoise(:, 3:4)];
+    else
+       fullS(counter, :, :) = [lmcosiS(:, 1:2) ...
+                               squeeze(lmcosiSSD(counter, :, 3:4))];
+    end
+    counter = counter + 1;
+end
+% fullS holds the combined synthetic signal and synthetic noise
+casetime = toc;
+disp(['Elapsed time for this case was ' num2str(casetime) ' seconds']);
 
 % Prepare outputs
 varns={G,V,ronmosiW,dems,dels,mz,ronm,mzin};
