a4e851ff81fa02fbda8cdb4067c0afa7ceb94808
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-26 12:59:56 -0700

    Got slept to not be all zeros as of line 168 in case A.  Next going to work through the propogation chain and see if I can fix it.

--- SyntheticCaseA.m
+++ SyntheticCaseA.m
@@ -165,7 +165,6 @@ for L=Ls
             falpha=G'*lmcosi(2*size(lmcosi,1)+ronm(1:(L+1)^2)-1);
             slept(k,:)=falpha;
         end
-        slept
 
         for h=1:length(truncations)
            if numfun(h) > 0
