5d3695aee2beb362221a0c3c57b64fcab67857fc
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-26 14:41:35 -0700

    Fixed another typo, for grace2slept.  But note that slept is still all zeros at this point.  Still need to pin down why that is the case.

--- SyntheticCaseA.m
+++ SyntheticCaseA.m
@@ -167,7 +167,6 @@ for L=Ls
             falpha=G'*lmcosi(2*size(lmcosi,1)+ronm(1:(L+1)^2));
             slept(k,:)=falpha;
         end
-        slept
         for h=1:length(truncations)
             if numfun(h) > 0
                 % Estimate the total mass change
