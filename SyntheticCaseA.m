e371d57ebf22a23c04d396c1053d87938ade854b
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-26 17:49:12 -0700

    auto-saving in applychanges.sh script

--- SyntheticCaseA.m
+++ SyntheticCaseA.m
@@ -153,6 +153,7 @@ for L=Ls
             if size(lmcosi,1) < addmup(L)
                 lmcosi=[lmcosi; lmcosipad(size(lmcosi,1)+1:end,:)];
             else
+                addmup(L)
                 lmcosi=lmcosi(1:addmup(L),:);
             end
   
@@ -167,8 +168,6 @@ for L=Ls
             falpha=G'*lmcosi(2*size(lmcosi,1)+ronm(1:(L+1)^2));
             slept(k,:)=falpha;
         end
-        slept
-        numfun(h)
         for h=1:length(truncations)
             if numfun(h) > 0
                 % Estimate the total mass change
