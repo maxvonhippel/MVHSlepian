0728ac57606827514fb6847b3df0614eef835823
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-26 17:42:32 -0700

    auto-saving in applychanges.sh script

--- SyntheticCaseA.m
+++ SyntheticCaseA.m
@@ -137,7 +137,7 @@ for L=Ls
         % We want the G from glmalpha, but we also want the eigenfunctions,
         % so use grace2slept to load both
         %[G,V,EL,EM,N]=glmalpha(TH,L,[],0);
-        [~,~,~,XY,G,CC]=grace2slept('CSRRL05',XY,XY_buffer,L,[],[],[],[],'SD',0);
+        [~,~,~,XY,G,CC]=grace2slept('CSRRL05',XY,XY_buffer,L,[],[],[],'N','SD',0);
 
         % Get the mapping from LMCOSI into not-block-sorted GLMALPHA
         [~,~,~,lmcosipad,~,~,~,~,~,ronm]=addmon(L);
