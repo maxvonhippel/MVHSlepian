04708d48c59a2bc0f8817ce6a21ac4c98ae58621
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-26 18:08:30 -0700

    auto-saving in applychanges.sh script

--- SyntheticCaseA.m
+++ SyntheticCaseA.m
@@ -170,7 +170,7 @@ for L=Ls
             % In terms of debugging what's wrong here (why slept all 0s),
             % I think this line might be the most important one: 
             falpha=G'*lmcosi(2*size(lmcosi,1)+ronm(1:(L+1)^2));
-            ronm(1:(L+1)^2)
+            2*size(lmcosi,1)+ronm(1:(L+1)^2)
             %  ----------------------------------------------------------
             %  ----------------------------------------------------------
 
