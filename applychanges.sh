49eee314c0b84412145f56b7c8cfe0e4db496b1b
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-28 13:04:22 -0700

    auto-saving in applychanges.sh script

--- applychanges.sh
+++ applychanges.sh
@@ -6,6 +6,7 @@ cp SyntheticCaseA.m /Users/maxvonhippel/Slepian/SyntheticCaseA.m &&
 cp SyntheticExperiments.m /Users/maxvonhippel/Slepian/SyntheticExperiments.m &&
 cp SyntheticCaseAA.m /Users/maxvonhippel/Slepian/SyntheticCaseAA.m &&
 cp slept2resid.m /Users/maxvonhippel/Slepian/slepian_delta/slept2resid.m &&
+cp iceland.m /Users/maxvonhippel/Slepian/slepian_alpha/REGIONS/iceland.m &&
 git add . && git commit -m "auto-saving in applychanges.sh script" &&
 say "COPIED PROGRESS";
 
