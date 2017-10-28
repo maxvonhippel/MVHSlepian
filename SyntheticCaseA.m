7d064cd307d2b186eb04deef8ed6789cf3402304
Max von Hippel <maxvonhippel1996@gmail.com>
2017-10-28 14:45:01 -0700

    auto-saving in applychanges.sh script

--- SyntheticCaseA.m
+++ SyntheticCaseA.m
@@ -103,10 +103,10 @@ for k=deltadates
     % syntheticnoise=[lmcosidata(:,1:2) temp1];
     % Add this to the signal
     % if wantnoise
-    fullS(counter,:,:)=[lmcosidata(:,1:2)...
-          squeeze(lmcosiSSD(counter,:,3:4))+syntheticnoise(:,3:4)];
+    %   fullS(counter,:,:)=[lmcosidata(:,1:2)...
+    %       squeeze(lmcosiSSD(counter,:,3:4))+syntheticnoise(:,3:4)];
     % else
-    %    fullS(counter,:,:)=[lmcosiS(:,1:2) squeeze(lmcosiSSD(counter,:,3:4))];
+    fullS(counter,:,:)=[lmcosiS(:,1:2) squeeze(lmcosiSSD(counter,:,3:4))];
     % end
     counter=counter+1;
 end
