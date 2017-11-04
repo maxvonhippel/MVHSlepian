e005d406a4437005ff7a10da49be757d4824a86e
Max von Hippel <maxvonhippel1996@gmail.com>
2017-11-04 16:23:01 -0700

    auto-saving in applychanges.sh script

--- SyntheticCase.m
+++ SyntheticCase.m
@@ -184,7 +184,7 @@ for L=Ls
                  totalparams,totalparamerrors,totalfit,functionintegrals,...
                  alphavar]=slept2resid(slept,thedates,[3 30 180 365.0],...
                                        [],[],CC,TH,numfun(h));
-                allslopes{h}(counter)=totalparams;
+                allslopes{h}(counter)=totalparams; %(2)*365;
             else
                 allslopes{h}(counter)=NaN;
             end
