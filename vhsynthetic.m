f984a052fe60ec30256b071974db76ded8f59195
Max von Hippel <maxvonhippel1996@gmail.com>
2017-11-29 23:06:41 -0700

    auto-saving in applychanges.sh script

--- vhsynthetic.m
+++ vhsynthetic.m
@@ -128,7 +128,8 @@ else
 		dom2=dom1;
 	end
 	% Make a synthetic unit signal over the region
-	[~,~,~,~,~,lmcosiS]=geoboxcap(Ldata,dom2);
+	[~,~,~,~,~,lmcosiS]=geoboxcap(Ldata,dom2,[],10);
+	% [~,~,~,~,~,lmcosiS]=geoboxcap(Ldata,dom2,N,degres,act,lonc,latc)
 	% Convert desired Gt/yr to kg/yr
 	factor1=Signal*10^12;
 	% Then get an average needed for the region (area in meters)
