7b85e0af8134859cb9afb8f799b7d4a1e63703eb
Max von Hippel <maxvonhippel1996@gmail.com>
2018-01-14 15:55:03 -0700

    Working on figures

--- Spring18/hs12syntheticrecovery.m
+++ Spring18/hs12syntheticrecovery.m
@@ -3,17 +3,33 @@ function hs12syntheticrecovery
 % Here we plot the contour of recovered trends from synthetic experiments
 % 
 % Authored by maxvonhippel at email.arizona.edu on 01/11/18
+% Last modified by maxvonhippel at email.arizona.edu on 01/14/18
 
 defval('domSignal','greenland');
-defval('domRecover',domSignal)
+defval('wantnoise',1);
+defval('domRecover','iceland')
 defval('Signal',200);
 defval('Ls',[20 25 30 35 40 45 50 55 60]);
 defval('buffers',[0 0.5 1 1.5 2 2.5 3]);
 defval('Ldata',60);
 
-% Apply a unit signal over the domain
+% Get the original data
 [potcoffs,~,thedates]=grace2plmt('CSR','RL05','SD',0);
 nmonths=length(thedates);
+% Get the fitted results
+[ESTresid,~,~,~,~,~]=plmt2resid(potcoffs(:,:,1:4),...
+    thedates,[1 1 181.0 365.0]);
+[Clmlmp,~,~,~,~]=plmresid2cov(ESTresid,Ldata,[]);
+[~,~,~,lmcosidata,~,~,~,~,~,ronmdata]=addmon(Ldata);
+% Decompose the covariance matrix
+T=cholcov(Clmlmp);
+[n,m]=size(T);
+if isempty(T)
+  disp('Empty covariance matrix, something is wrong.');
+  return
+end
+
+% Apply a unit signal over the domain
 [~,~,~,~,~,lmcosiS]=geoboxcap(Ldata,domSignal);
 factor1=Signal*10^12/spharea(domSignal)/4/pi/6370000^2/365;
 deltadates=thedates-thedates(1);
@@ -22,39 +38,58 @@ counter=1;
 for k=deltadates
   factor2=(factor1*k);
   lmcosiSSD(counter,:,:)=[lmcosiS(:,1:2) lmcosiS(:,3:4)*factor2];
+  if wantnoise
+      % Make a synthetic noise realization
+      syntheticnoise=randn(1,n)*T;
+      % Reorder the noise
+      temp1=lmcosidata(:,3:4);
+      temp1(ronmdata)=syntheticnoise(:);
+      syntheticnoise=[lmcosidata(:,1:2) temp1];
+      fullS(counter,:,:)=[lmcosidata(:,1:2)...
+         squeeze(lmcosiSSD(counter,:,3:4))+syntheticnoise(:,3:4)];
+  else
+      fullS(counter,:,:)=[lmcosiS(:,1:2) squeeze(lmcosiSSD(counter,:,3:4))];
+  end
   counter=counter+1;
 end
 
+if ~wantnoise
+  fullS=lmcosiSSD;
+end
+
 % Recover the signal from the recovery domain
-fullS=lmcosiSSD;
 slopes=zeros([(length(Ls)*length(buffers)) 3]);
 overallcount=1;
 
 for L=Ls
   for B=buffers
-    TH={domRecover B};
-    % We want the G from glmalpha, but we also want the eigenfunctions,
-    % so use grace2slept to load both
-    [slepcoffs,~,~,TH,G,CC,~,~]=grace2slept('CSRRL05',domRecover,B,L,...
-    										[],[],[],[],'SD',1);
-    [~,~,~,lmcosipad,~,~,~,~,~,ronm]=addmon(L);
-    slept=zeros(nmonths,(L+1)^2);
-    for k=1:nmonths
-    	lmcosi=squeeze(fullS(k,:,:));
-      	if size(lmcosi,1) < addmup(L)
-      		lmcosi=[lmcosi; lmcosipad(size(lmcosi,1)+1:end,:)];
-      	else
+    try
+      TH={domRecover B};
+      % We want the G from glmalpha, but we also want the eigenfunctions,
+      % so use grace2slept to load both
+      [slepcoffs,~,~,TH,G,CC,~,~]=grace2slept('CSRRL05',domRecover,B,L,...
+      										[],[],[],[],'SD',1);
+      [~,~,~,lmcosipad,~,~,~,~,~,ronm]=addmon(L);
+      slept=zeros(nmonths,(L+1)^2);
+      for k=1:nmonths
+      	lmcosi=squeeze(fullS(k,:,:));
+        if size(lmcosi,1) < addmup(L)
+        	lmcosi=[lmcosi; lmcosipad(size(lmcosi,1)+1:end,:)];
+        else
           	lmcosi=lmcosi(1:addmup(L),:);
-      	end
-    	slept(k,:)=G'*lmcosi(2*size(lmcosi,1)+ronm(1:(L+1)^2));
+        end
+      	slept(k,:)=G'*lmcosi(2*size(lmcosi,1)+ronm(1:(L+1)^2));
+      end
+      % Estimate the total mass change
+      [ESTsignal,ESTresid,tests,extravalues,total,alphavarall,...
+       totalparams,totalparamerrors,totalfit,functionintegrals,...
+       alphavar]=slept2resid(slept,thedates,[1 365.0],[],[],CC,TH);
+      % Index allslopes by L and B
+      format long g;
+      result=[L B totalparams(2)*365];
+    catch
+      result=[L B 0];
     end
-    % Estimate the total mass change
-    [ESTsignal,ESTresid,tests,extravalues,total,alphavarall,...
-     totalparams,totalparamerrors,totalfit,functionintegrals,...
-     alphavar]=slept2resid(slept,thedates,[1 365.0],[],[],CC,TH);
-    % Index allslopes by L and B
-    format long g;
-    result=[L B totalparams(2)*365];
     slopes(overallcount,:)=result;
     overallcount=overallcount+1;
   end
