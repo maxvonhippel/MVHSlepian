652b9af3307004c9021d563785df6d4580c4e071
Max von Hippel <maxvonhippel1996@gmail.com>
2018-02-18 11:09:18 -0700

    Working on fancycontour routine for real figures

---
+++ Spring18/fancycontour.m
@@ -0,0 +1,57 @@
+function fancycontour(realFile,syntheticFile,regionName)
+% Example:
+% [slopes]=hs12realrecovery('GREENLAND_REAL.dat','GG_WITH_NOISE.dat',...
+% 							'Greenland');
+% 
+% Here we plot the contour of recovered trends from real data and overlay
+% the 100% contour from the synthetic experiments.  Then we export the figure
+% to be a well-formatted PDF which we can insert into our LATEX document
+% down the line.
+% 
+% Authored by maxvonhippel-at-email.arizona.edu on 02/15/18
+
+% Do we have any inputs?
+if not(exist('realFile','var')) | not(exist('syntheticFile','var'))
+	disp('Please provide realFile and syntheticFile arguments to script')
+	return
+end
+
+% Read in the input files
+% Solution adapted from: 
+% https://www.mathworks.com/matlabcentral/answers/
+% 	79885-reading-dat-files-into-matlab
+% First, real data.
+reald=fopen(realFile,'r');
+data=textscan(reald,'%f%f%f','HeaderLines',1,'Collect',1);
+fclose(reald);
+reald=data{1};
+% Next, synthetic data.
+synthd=fopen(syntheticFile,'r');
+data=textscan(synthd,'%f%f%f','HeaderLines',1,'Collect',1);
+fclose(reald);
+synthd=data{1};
+
+% Next, we want to draw the real recovery figure.
+Ls=reald(:,1);
+buffers=reald(:,2);
+recovered=reald(:,3);
+% Ranges of axes
+LsRange=min(Ls):(max(Ls)-min(Ls))/200:max(Ls);
+buffersRange=min(buffers):(max(buffers)-min(buffers))/200:max(buffers);
+% Contour data
+[LsRange, buffersRange]=meshgrid(LsRange,buffersRange);
+recoveredTrends=griddata(Ls,buffers,recovered,LsRange,buffersRange);
+% Chart it
+contour(LsRange,buffersRange,recoveredTrends,5,'ShowText','On');
+% Next, we want to add the 100% contour from the synthetic data
+LsSynthetic=synthd(:,1);
+buffersSynthetic=synthd(:,2);
+recoveredSynthetic=synthd(:,3);
+percentRecovered=griddata(LsSynthetic,buffersSynthetic,recoveredSynthetic,...
+	LsRange,buffersRange);
+contour(LsRange,buffersRange,percentRecovered,[100,100],'ShowText','Off',...
+   'LineColor','Green','LineWidth',2);
+% Add title and axes
+title(sprintf('GRACE data trend (Gt/yr) over %s',regionName));
+xlabel('bandwidth L');
+ylabel('buffer extent (degrees)');
