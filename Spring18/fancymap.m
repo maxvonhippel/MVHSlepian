25919dc03e514e965875850e06cfcc8388bc07b6
Max von Hippel <maxvonhippel1996@gmail.com>
2018-05-28 17:09:12 -0700

    Worked a bunch on the slepian functions figures:

--- Spring18/fancymap.m
+++ Spring18/fancymap.m
@@ -88,3 +88,43 @@ fprintf(fp,'%.4f %.4f %.4f\n',lon(lo),lat(la),rtotal(la,lo));
 end
 end
 fclose(fp);
+
+% Make the slepian functions maps (one for each function)
+dom='greenland';
+b=0.5;
+[slept,~,thedates,TH,G,CC,V,N]=grace2slept(...
+	'CSRRL05',dom,b,60,[],[],[],[],'SD',1);
+[V,vi]=sort(V,'descend');
+thedates=thedates(1:157);
+slept=slept(1:157,:);
+[thedates,PGRt]=correct4gia(thedates,'Paulson07',TH,60);
+sleptcorrected=slept-PGRt;
+[ESTsignal,ESTresid,ftests,extravalues,total,alphavarall,totalparams,...
+	totalparamerrors,totalfit,functionintegrals,alphavar]...
+	=slept2resid(slept,thedates,[3 182.625 365.25],[],[],CC,TH);
+ESTsignalDelta=ESTsignal(157,:)-ESTsignal(1,:);
+totalmap=CC{1}(:,3:4)*ESTsignalDelta(1)/10;
+totalmap=[CC{1}(:,1:2) totalmap];
+[rtotal,lon,lat]=plm2xyz(totalmap,1);
+fp=fopen(sprintf('%s_slep_1.dat',dom),'wt');
+fprintf(fp,'lon lat total\n');
+for lo=1:numel(lon)
+	for la=1:numel(lat)
+		fprintf(fp,'%.4f %.4f %.4f\n',lon(lo),lat(la),rtotal(la,lo));
+	end
+end
+fclose(fp);
+N=round(N);
+for j=2:N
+    totalmap=CC{j}(:,3:4)*ESTsignalDelta(j)/10;
+    totalmap=[CC{1}(:,1:2) totalmap];
+	[rtotal,lon,lat]=plm2xyz(totalmap,1);
+    fp=fopen(sprintf('%s_slep_%d.dat',dom,j),'wt');
+    fprintf(fp,'lon lat total\n');
+	for lo=1:numel(lon)
+		for la=1:numel(lat)
+			fprintf(fp,'%.4f %.4f %.4f\n',lon(lo),lat(la),rtotal(la,lo));
+		end
+	end
+	fclose(fp);
+end
